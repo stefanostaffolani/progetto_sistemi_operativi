294c52aa57b35a277fb2330ec35fa74a2efeabea
CiumbaSpe <filippo.speziali01@gmail.com>
2022-02-20 16:11:54 +0100

    update

--- src/phase1/pcb.c
+++ src/phase1/pcb.c
@@ -1,5 +1,10 @@
 #include "pcb.h"
 
+/*
+*   Per l'implementazione della coda dei processi, inseriamo in testa con list_add(&head)
+*    e rimuoviamo in coda con list_del(head.prev)
+*/
+
 static struct list_head pcbFree_h;
 
 static pcb_t pcbFree_table[MAXPROC];
@@ -12,18 +17,18 @@ essere chiamato una volta sola in fase di
 inizializzazione della struttura dati.
 */
 
-void initPcbs(){ //TODO
+void initPcbs(){    
     int i = 0;
-    INIT_LIST_HEAD(&pcbFree_h);
+    INIT_LIST_HEAD(&pcbFree_h);                                  // inizializzo la pcbFree_h (lista di processi liberi)
     while(i < MAXPROC){
-        list_add_tail(&(pcbFree_table[i].p_list), &pcbFree_h);   //aggiunge in coda
-        INIT_LIST_HEAD(&(pcbFree_table[i].p_child));
+        list_add_tail(&(pcbFree_table[i].p_list), &pcbFree_h);   // aggiunge in coda nella pcbFree
+        INIT_LIST_HEAD(&(pcbFree_table[i].p_child));             // inizializzo la lista dei figli
         i++;
     }
 }
 
 /*
-nserisce il PCB puntato da p nella lista
+inserisce il PCB puntato da p nella lista
 dei PCB liberi (pcbFree_h)
 */
 
@@ -39,17 +44,17 @@ e restituisce l’elemento rimosso.
 */
 
 pcb_PTR allocPcb(){
-    if(list_empty(&pcbFree_h)){
-        return NULL;
+    if(list_empty(&pcbFree_h)){         // se la pcbFree è vuota 
+        return NULL;                    // restituisco NULL (non ci sono processi)
     }
-    pcb_PTR tmp =  container_of(pcbFree_h.next, pcb_t, p_list);
-    //tmp->p_child.next = NULL;
-    list_del(list_next(&pcbFree_h));
-    INIT_LIST_HEAD(&tmp->p_child);
+    pcb_PTR tmp = container_of(pcbFree_h.next, pcb_t, p_list);      // tmp puntatore al primo processo nella lista di quelli liberi
+    list_del(list_next(&pcbFree_h));                                // rimuovo il primo elemento di quelli liberi
+    INIT_LIST_HEAD(&tmp->p_child);                                  // inizializzo i campi di tmp                       
     INIT_LIST_HEAD(&tmp->p_list);
-    tmp->p_parent = NULL;
+    tmp->p_parent = NULL;                                               
     INIT_LIST_HEAD(&tmp->p_sib);
-    return tmp;
+    tmp->p_semAdd = NULL;
+    return tmp;                                                     // restituisco il primo processo nella lista di quelli liberi
 }
 
 /*
@@ -81,8 +86,8 @@ pcb_PTR headProcQ(struct list_head *head){
     if(list_empty(head)){
         return NULL;
     }
-    pcb_PTR ret_val = container_of(head->prev, pcb_t, p_list);
-    return ret_val;
+    pcb_PTR ret_val = container_of(head->prev, pcb_t, p_list);      
+    return ret_val;     // restituisce l'ultimo elemento della coda (primo processo inserito)
 }
 
 /*
@@ -120,15 +125,15 @@ trovarsi in una posizione arbitraria della coda).
 pcb_PTR outProcQ(struct list_head* head, pcb_t *p){
     struct list_head *iter;
     int found = FALSE;
-    list_for_each(iter, head){
-        if(iter == &(p->p_list)){
-            list_del(iter);
-            found = TRUE;
-            break;
+    list_for_each(iter, head){              
+        if(iter == &(p->p_list)){       // se trovo il processo p che ha come campo p_list iter
+            list_del(iter);             // lo rimuovo dalla lista
+            found = TRUE;                          
+            break;  
         }
     }
-    if(!found) return NULL;
-    else return container_of(iter,pcb_t, p_list);
+    if(!found) return NULL;                         
+    else return container_of(iter,pcb_t, p_list);   // restituisco il processo che ha come campo iter
 }
 
 /*
@@ -149,12 +154,12 @@ del PCB puntato da prnt.
 */
 
 void insertChild(pcb_PTR pnrt, pcb_PTR p){
-    if(list_empty(&pnrt->p_child)){
-        pnrt->p_child.next = &p->p_sib;
-        p->p_parent = pnrt;
+    if(list_empty(&pnrt->p_child)){                     // se non ha figli
+        pnrt->p_child.next = &p->p_sib;                 // il puntatore di child.next punta a p->p_sib (lo inserisco in testa)
+        p->p_parent = pnrt;                             
     }else{
-        list_add_tail(&p->p_sib, &pnrt->p_child);
-        p->p_parent = pnrt;
+        list_add_tail(&p->p_sib, &pnrt->p_child);       // lo aggiungo in coda
+        p->p_parent = pnrt;                             
     }
 }
 
@@ -164,13 +169,13 @@ da p. Se p non ha figli, restituisce NULL.
 */
 
 pcb_PTR removeChild(pcb_PTR p){
-    if(list_empty(&p->p_child)){
-        return NULL;
+    if(list_empty(&p->p_child)){                                //  Se p non ha figli
+        return NULL;                                            //  restituisce NULL
     }
-    pcb_PTR son = container_of(p->p_child.next, pcb_t, p_sib);
-    list_del(p->p_child.next);
-    p->p_parent = NULL;
-    return son;
+    pcb_PTR son = container_of(p->p_child.next, pcb_t, p_sib);  //  primo processo figlio
+    list_del(p->p_child.next);                                  //  rimuovo il primo figlio della coda
+    p->p_parent = NULL;                                         
+    return son;                                                 //  restituisco il figlio rimosso
 }
 
 /*
@@ -185,14 +190,9 @@ necessariamente il primo figlio del
 padre).
 */
 
-pcb_PTR outChild(pcb_PTR p){
-    pcb_PTR daddy = p->p_parent;
-    if(daddy == NULL) return NULL;
-    pcb_PTR sib = container_of(p->p_sib.next, pcb_t, p_sib);
-    list_del(&p->p_sib);
-    if(daddy->p_child.next == &p->p_child){    //se e' il primo figlio
-        list_del(&p->p_child);
-        daddy->p_child.next = &sib->p_child;
-    }
+pcb_PTR outChild(pcb_PTR p){    
+    pcb_PTR daddy = p->p_parent;                                // processo padre
+    if(daddy == NULL) return NULL;                              // se è NULL ritorna NULL
+    list_del(&p->p_sib);                                        // rimuovo p dalla lista dei fratelli
     return p;
 }
